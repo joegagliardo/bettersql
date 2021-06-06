846a9cfffc423359814296904800ec5ab9983c5f
Joseph Gagliardo <joey@me.com>
2021-06-06 17:41:22 -0400

    1.1.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.1.2' 
+VERSION = '1.1.3' 
 DESCRIPTION = 'A package to use SQL with Pandas DataFrames, list and dict.'
 LONG_DESCRIPTION = 'A more feature rich alternative to Pandasql that allows for custom UDF, as well as lit and dict as source tables and return types'
 
