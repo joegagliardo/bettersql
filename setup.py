8435f56c00106695e8ec5102f5c57b13591cdaaf
Joseph Gagliardo <joey@me.com>
2021-06-06 11:37:20 -0400

    1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ LONG_DESCRIPTION = 'A more feature rich alternative to Pandasql that allows for 
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 # Setting up
